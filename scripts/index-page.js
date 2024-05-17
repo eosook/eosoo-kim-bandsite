5a3e3ac0df8fe56069d08cccce892e8bc3f06173
Eo Soo Kim <eosookim@gmail.com>
2024-05-17 16:28:43 -0400

    add shows section and add comments to section using javascript

---
+++ scripts/index-page.js
@@ -0,0 +1,41 @@
+let comments = [
+    {
+        name: "Victor Pinto",
+        comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+        date: "11/02/2023"
+    },
+    {
+        name: "Christina Cabrera",
+        comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+        date: "10/28/2023"
+    },
+    {
+        name: "Isaac Tadesse",
+        comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+        date: "10/20/2023"
+    }
+]
+
+const commentSection = document.querySelector(".comments");
+
+function createCommentElement(type, className, text, parent){
+    let commentElement = document.createElement(type);
+    commentElement.classList.add(className);
+    commentElement.innerText = text;
+    parent.append(commentElement);
+}
+
+function createComment(comment){
+    let commentContainer = document.createElement("div");
+    let commentDiv = document.createElement("div");
+    commentDiv.classList.add("comments--flex");
+    createCommentElement("p", "comments__name", comment.name, commentDiv);
+    createCommentElement("p", "comments__date", comment.date, commentDiv);
+    commentContainer.appendChild(commentDiv);
+    createCommentElement("p", "comments__comment", comment.comment, commentContainer);
+    commentSection.append(commentContainer);
+}
+
+comments.forEach((comment) => {
+    createComment(comment);
+})
