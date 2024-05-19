8187a2b5d488d2518fbc163f520c42c6f43fb20f
Eo Soo Kim <eosookim@gmail.com>
2024-05-19 14:04:17 -0400

    submit button now clears all comments and re renders all comments from array

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,8 +1,9 @@
 let comments = [
     {
-        name: "Isaac Tadesse",
-        comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-        date: "10/20/2023"
+        
+        name: "Victor Pinto",
+        comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+        date: "11/02/2023"
     },
     {
         name: "Christina Cabrera",
@@ -10,9 +11,9 @@ let comments = [
         date: "10/28/2023"
     },
     {
-        name: "Victor Pinto",
-        comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-        date: "11/02/2023"
+        name: "Isaac Tadesse",
+        comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+        date: "10/20/2023"
     }
 ]
 
@@ -39,12 +40,16 @@ function createComment(comment){
     commentContainerInfo.appendChild(commentDiv);
     createCommentElement("p", "comments__comment", comment.comment, commentContainerInfo);
     commentContainer.append(commentContainerInfo);
-    commentSection.prepend(commentContainer);
+    commentSection.append(commentContainer);
 }
 
-comments.forEach((comment) => {
-    createComment(comment);
-})
+function createCommentSection(){
+    comments.forEach((comment) => {
+        createComment(comment);
+    })
+}
+
+createCommentSection();
 
 commentForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -54,5 +59,7 @@ commentForm.addEventListener("submit", (e) => {
         comment: e.target.personComment.value,
         date: `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
     })
-    createComment(comments[0]);
+    commentSection.innerHTML = "";
+    createCommentSection();
+    commentForm.reset();
 })
