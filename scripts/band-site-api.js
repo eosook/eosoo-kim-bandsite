d5eb70824ae6cdc0e8ca5e69474b041fcab3db1e
Eo Soo Kim <eosookim@gmail.com>
2024-05-21 15:54:33 -0400

    create api script and begin making bandSiteApi class

---
+++ scripts/band-site-api.js
@@ -0,0 +1,18 @@
+class BandSiteApi {
+    constructor(apiKey){
+        this.apiKey = apiKey;
+        this.baseUrl = "https://unit-2-project-api-25c1595833b2.herokuapp.com/";
+    }
+
+    postComment(comment){
+
+    }
+
+    getComments(){
+
+    }
+
+    getShows(){
+        
+    }
+}
