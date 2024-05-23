0b64d1d03e7fa3b117cb7ce9d4ffe542747b3d3b
Eo Soo Kim <eosookim@gmail.com>
2024-05-23 14:20:36 -0400

    create bandsite api module and use getComments method to get comments list

--- scripts/band-site-api.js
+++ scripts/band-site-api.js
@@ -1,18 +1,22 @@
 class BandSiteApi {
     constructor(apiKey){
         this.apiKey = apiKey;
-        this.baseUrl = "https://unit-2-project-api-25c1595833b2.herokuapp.com/";
+        this.baseUrl = "https://unit-2-project-api-25c1595833b2.herokuapp.com";
     }
 
     postComment(comment){
-
+        console.log("hello");
     }
 
-    getComments(){
-
+    async getComments(){
+        const commentsList = await axios.get(`${this.baseUrl}/comments?api_key=${this.apiKey}`);
+        return commentsList.data;
     }
 
     getShows(){
         
     }
 }
+
+export default new BandSiteApi();
+export {BandSiteApi};
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,24 +1,28 @@
-let comments = [
-    {
+// let comments = [
+//     {
         
-        name: "Victor Pinto",
-        comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-        date: "11/02/2023"
-    },
-    {
-        name: "Christina Cabrera",
-        comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-        date: "10/28/2023"
-    },
-    {
-        name: "Isaac Tadesse",
-        comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-        date: "10/20/2023"
-    }
-]
+//         name: "Victor Pinto",
+//         comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+//         date: "11/02/2023"
+//     },
+//     {
+//         name: "Christina Cabrera",
+//         comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+//         date: "10/28/2023"
+//     },
+//     {
+//         name: "Isaac Tadesse",
+//         comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+//         date: "10/20/2023"
+//     }
+// ]
+import {BandSiteApi} from "./band-site-api.js";
 
 const commentSection = document.querySelector(".comments__list");
 const commentForm = document.querySelector(".form");
+
+const myApiKey = "7f3235f9-3920-4ab5-9c90-59585878113a";
+let bandSiteData = new BandSiteApi(myApiKey);
 
 function createCommentElement(type, className, text, parent){
     let commentElement = document.createElement(type);
@@ -43,13 +47,11 @@ function createComment(comment){
     commentSection.append(commentContainer);
 }
 
-function createCommentSection(){
+function createCommentSection(comments){
     comments.forEach((comment) => {
         createComment(comment);
     })
 }
-
-createCommentSection();
 
 commentForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -63,3 +65,10 @@ commentForm.addEventListener("submit", (e) => {
     createCommentSection();
     commentForm.reset();
 })
+
+async function getComments(){
+    const comments = await bandSiteData.getComments();
+    createCommentSection(comments);
+}
+
+getComments();
